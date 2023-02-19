6af6dcca38a8b21dc20baa0eb3595a8a6791fa32
Jadwiga Świerczyńska <jadwiga.swierczynska22@gmail.com>
2023-02-19 17:32:57 +0100

    Added documentation for functions in modules.

--- exchange.js
+++ exchange.js
@@ -2,6 +2,10 @@ var http = require('http');
 var request = require('request');
 const NBP = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json"
 
+/**
+ * Returns Promise, which contains current exchange rate fetched from NBP website.
+ */
+
 var GetExchangeRate = async () => {
     return new Promise((res, rej) => {
         request(NBP, (err, resp, body) => {
--- parser.js
+++ parser.js
@@ -1,12 +1,34 @@
+
+/**
+ * Checks whether given string matches number pattern. Returns true, when
+ * - s contains only digits
+ * - or s contains only digits and one '.' (dot) - but the dot is neither the first, nor the last character in the string
+ * 
+ * and false otherwise.
+ * @param {string} s 
+ * @returns {boolean}
+ */
 function MatchesNumberPattern(s) {
     return (/^\d+$/.test(s) || /^\d+\.\d+$/.test(s));
 }
 
+/**
+ * Reverses given string.
+ * @param {string} s 
+ * @returns {string}
+ */
 function ReverseString(s) {
     if( !s ) return "";
     return s.split("").reverse().join("");
 }
 
+/**
+ * Calculates value of a string, assuming that it is a number given in a base. Multiplies final result by mult.
+ * @param {string} s 
+ * @param {number} base 
+ * @param {number} mult 
+ * @returns {number}
+ */
 function ToNumber(s, base, mult){
     if( s === "" ) return 0;
 
@@ -19,6 +41,12 @@ function ToNumber(s, base, mult){
     }
     return res*base;
 }
+
+/**
+ * If given argument is not valid (i.e. is not a string or MatchesNumberPattern(s) === false), returns false. Otherwise calculates value of a number represented by a given string.
+ * @param {string} s 
+ * @returns {number | boolean}
+ */
 
 function Parser(s) {
     if( typeof s !== "string" || s === "" || !MatchesNumberPattern(s) ) {
